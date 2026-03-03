f8d8ac27ff9da38209a21190f55b14073d849f15
Afhaam <afhaamcoc@gmail.com>
2026-03-03 15:27:42 +0530

    Xpath and functions

---
+++ src/test/java/org/example/ex04_Selenium_Xpath/TS016_Mini_project__Xpath_Function.java
@@ -0,0 +1,39 @@
+package org.example.ex04_Selenium_Xpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TS016_Mini_project__Xpath_Function {
+
+    @Test
+    public void Ketalon_Cura() throws InterruptedException {
+
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+
+        Thread.sleep(1000);
+
+        WebElement appointment_btn = driver.findElement(By.xpath("//a[text()='Make Appointment']"));
+        appointment_btn.click();
+        Thread.sleep(1000);
+
+
+        WebElement email_input_box = driver.findElement(By.xpath("//input[contains(@id,'username')]"));
+        email_input_box.sendKeys("John Doe");
+
+       WebElement password_input_box = driver.findElement(By.xpath("//input[contains(@id,'password')]"));
+       password_input_box.sendKeys("ThisIsNotAPassword");
+
+        WebElement login_btn = driver.findElement(By.xpath("//button[text()='Login']"));
+        login_btn.click();
+        Thread.sleep(1000);
+
+        driver.quit();
+
+
+
+    }
+}
