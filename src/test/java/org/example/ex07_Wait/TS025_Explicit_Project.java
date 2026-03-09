76532ec42b833e0f8c3365343ee4e6c158279712
Mubashira A <mubashiramhm@gmail.com>
2026-03-09 16:53:07 +0530

    SVG

--- src/test/java/org/example/ex07_Wait/TS025_Explicit_Project.java
+++ src/test/java/org/example/ex07_Wait/TS025_Explicit_Project.java
@@ -1,10 +1,10 @@
 package org.example.ex07_Wait;
 
+import org.example.Utilities.Common_To_All;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -12,29 +12,35 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class TS025_Explicit_Project {
+public class TS025_Explicit_Project extends Common_To_All {
     @Test
     public void App_Vwo_Explicit() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://app.vwo.com/#/login");
 
 
+
+
+         //WebElement email_input_box = driver.findElement(By.id("login-username"));
+        //email_input_box.sendKeys("admin@admin.com");
+        //WebElement password_input_box = driver.findElement(By.id("login-password"));
+        //password_input_box.sendKeys("admin");
+ //
+        inputBox(driver,"login-username","admin@admin.com" );
+        inputBox(driver, "login-password","admin");
+
+//wait
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-
-        WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-username")));
-        email.sendKeys("admin@admin.com");
-
-        WebElement password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-password")));
-        password.sendKeys("admin");
-
         WebElement login_btn = wait.until(ExpectedConditions.elementToBeClickable(By.id("js-login-btn")));
         login_btn.click();
 
         WebElement error_notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("js-notification-box")));
 
 
-        Assert.assertEquals(error_notification.getText(),"Your email, password, IP address or location did not match");
+        Assert.assertEquals(error_notification.getText(), "Your email, password, IP address or location did not match");
 
         driver.quit();
     }
+
+
 }
